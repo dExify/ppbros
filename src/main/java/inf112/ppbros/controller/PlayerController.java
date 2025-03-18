f8188028d9b8e7c695a9b26cdbcf7df331eab119
Ole Christian <olechr.sollid@icloud.com>
2025-03-18 13:42:54 +0100

    Removed unused controller imports/variables

--- src/main/java/inf112/ppbros/controller/PlayerController.java
+++ src/main/java/inf112/ppbros/controller/PlayerController.java
@@ -1,9 +1,6 @@
 package inf112.ppbros.controller;
 
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.Screen;
 
 import inf112.ppbros.model.GameModel;
 import inf112.ppbros.view.ScreenView;
@@ -13,9 +10,6 @@ import com.badlogic.gdx.Input;
 
 public class PlayerController extends InputAdapter {
     private GameModel gameModel;
-    private Texture playerTexture, enemyTexture;
-    private SpriteBatch batch;
-    private ScreenView gameView;
     
     // map size (should probably be moved somewhere else later)
     final float MAP_WIDTH = 480;
@@ -23,8 +17,6 @@ public class PlayerController extends InputAdapter {
     
     public PlayerController(GameModel gameModel, ScreenView gameView) {
         this.gameModel = gameModel;
-        this.gameView = gameView;
-        this.batch = new SpriteBatch();
         //this.playerTexture = new Texture(Gdx.files.internal("character.png"));
         //this.enemyTexture = new Texture(Gdx.files.internal("enemy.png"));
         Gdx.input.setInputProcessor(this);
