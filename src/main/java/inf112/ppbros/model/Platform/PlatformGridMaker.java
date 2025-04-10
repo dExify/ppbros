7676a99fb6cce11cc1e2c5f25f40f88fea4a227d
Luka.Silic <lusil6304@uib.no>
2025-04-10 11:16:13 +0200

    Refactor code and added javadocs

--- src/main/java/inf112/ppbros/model/Platform/PlatformGridMaker.java
+++ src/main/java/inf112/ppbros/model/Platform/PlatformGridMaker.java
@@ -10,6 +10,10 @@ public class PlatformGridMaker {
         platformMaker = new PlatformMaker();
     }
 
+    /**
+     * Returns a platformGrid object with a yPos value above the last platformGrid
+     * @return
+     */
     public PlatformGrid getNextPlatformGrid() {
         platformGridObject = new PlatformGrid(platformMaker, iteration);
         platformGridObject.buildGrid(25);
