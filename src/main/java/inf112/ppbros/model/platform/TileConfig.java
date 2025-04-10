6f6dec0106bc13c95252e21bfb93c8d611e1efb4
Ole Christian <olechr.sollid@icloud.com>
2025-04-10 16:31:41 +0200

    Changed capitalization on folder

---
+++ src/main/java/inf112/ppbros/model/platform/TileConfig.java
@@ -0,0 +1,15 @@
+package inf112.ppbros.model.platform;
+
+import com.badlogic.gdx.Gdx;
+
+public final class TileConfig {
+  public static final int GRID_WIDTH = 24;
+  public static final int GRID_HEIGHT = 14;
+  public static final int TILE_SIZE = Gdx.graphics.getWidth()/GRID_WIDTH;
+  public static final int PLATFORM_GRIDHEIGHT_PIXELS = TILE_SIZE * GRID_HEIGHT;
+  
+  private TileConfig() {
+    
+  }
+  
+}
